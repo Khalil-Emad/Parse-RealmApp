09692022f1eb730b73f5ec176399855d140aced0
Khalil-Emad <khalilemad26@outlook.com>
2022-08-19 00:15:31 +0200

    update

--- lib/models/Parse.dart
+++ lib/models/Parse.dart
@@ -2,12 +2,9 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 class ParseModel {
   static final appId = "com.voyce";
-  // final keyParseClientKey = "";
   static final serverUrl = "https://test.voycephone.com/parse/";
 
   static Future<void> initParse() async {
     await Parse().initialize(appId, serverUrl);
   }
-
-
 }
